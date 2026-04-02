63c630b13b61d77f6bdd933b72b8cffd2a90192e
MD.Shimul <md.shimuldiu@gmail.com>
2026-04-02 21:03:37 +0600

    add fs.js

---
+++ fs.js
@@ -0,0 +1,35 @@
+let fs=require('fs')
+/* fs.writeFile('note.txt','hello shimul ,you have less time',(err)=>{
+    if (err){
+        console.log('error')
+    }
+    else{
+        console.log('sucessful')
+    }
+}) */
+
+    /* fs.appendFile('Note.txt','you have only 8 month',(err)=>{
+        if (err){
+            console.log('error') 
+        }
+        else{
+            console.log('sucessful')
+        }
+    }) */
+/* fs.readFile('httpModule.js','utf-8',(err,data)=>{
+    if (err){
+        console.log('error')
+    }
+    else{
+        console.log(data)
+    }
+}) */
+/* fs.rename('note.txt','Note.txt',(err)=>{
+    if (err){
+        console.log('error')
+    }
+    else{
+        console.log('sucessful')
+    }
+}) */
+console.log(fs)
