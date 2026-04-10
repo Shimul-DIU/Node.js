c5dfbf00d611575a1590c3c1fccbaa6a87e751e5
MD.Shimul <md.shimuldiu@gmail.com>
2026-04-10 12:17:31 +0600

    application and route middleware

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 let express=require('express')
 let app=express()
 let path=require('path')
-
+app.use(express.static("public"))
 
 let userRouter=require('./routes/user.routes')
 const bodyParser=require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use("/user",userRouter)
+let middleWare=(req,res,next)=>{
+    req.currentTime=new Date()
+    console.log('middleware function call')
+    next()
+}
 
 
-
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/home.html'))
+app.get("/",middleWare,(req,res)=>{
+    // res.sendFile(path.join(__dirname,'./views/home.html'))
+    console.log('i am home router')
+    res.send('i am home and time'+req.currentTime)
+    
+    
 })
+
+
 app.get("/contact",(req,res)=>{
     res.status(202).json(
         {
