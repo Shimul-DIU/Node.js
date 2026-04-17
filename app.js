4b4b308a56c6d2b6c5e667060193e62b56950ffb
MD.Shimul <md.shimuldiu@gmail.com>
2026-04-17 22:12:29 +0600

    url setup for app.jsx

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express=require('express')
 let mongoose=require('mongoose')
+let path=require('path')
 const userRouter = require('./routes/userRoutes.js')
 let app=express()
 
@@ -11,6 +12,17 @@ let connectMongodb=async()=>{
         console.log('connect not sucessfull')
     }
 }
-app.use('/',userRouter)
-
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+app.use((req,res,next)=>{
+    res.status(404).send({
+        message:'404 not found'
+    })
+})
+app.use((err,req,res,next)=>{
+    res.status(500).send({
+        message:'server is busy'
+    })
+})
 module.exports = { app, connectMongodb }
